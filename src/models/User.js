fb7a26a5d04fc12e8bf716501332811098777ebf
marcosF <marofelipe@live.com>
2019-11-26 18:09:24 -0300

    apagando o protoripo em react

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    email: String,
-    nome: String,
-    NivelAdm: String,
-    Aluno: Array,       
-})
-module.exports = mongoose.model('User',UserSchema);
